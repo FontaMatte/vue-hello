bbc58534b1fd2301a940b946a7d322383dd28a0c
FontaMatte <matteo.fontanel@gmail.com>
2023-01-20 15:04:06 +0100

    Bonus1 completed

---
+++ Bonus1/js/script.js
@@ -0,0 +1,14 @@
+  const { createApp } = Vue;
+
+  createApp({
+    data() {
+      return {
+
+        info : {
+            message: "Hello Vue!",
+            imageUrl: "vue.png"
+
+        }
+      }
+    }
+  }).mount('#app')
